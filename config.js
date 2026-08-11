2d8b9e7cc4df9a7c3e784ae8cb4c3e9576518f23
johnjohn114 <e20121214@gmail.com>
2026-08-11 20:59:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ const SUPABASE_URL = "https://fsoowongtavlsrwvzbze.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_nAKu843QXRNajjS5F6EYGA_xxn9UlLb";
 
 // 測試用訪客密碼；正式網站建議改成伺服器端驗證。
-const DEMO_VISITOR_PASSWORD = "123456";
+const DEMO_VISITOR_PASSWORD = "20260811Open";
